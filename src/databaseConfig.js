ba82f2758b7321e5c76d67ab5bc30251faf7813a
Kivuti Njeru <kivutialex4@gmail.com>
2024-09-18 15:20:45 +0300

    🚀

--- src/databaseConfig.js
+++ src/databaseConfig.js
@@ -4,13 +4,13 @@ import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: "jsproject-6252d.firebaseapp.com",
-  databaseURL: "https://jsproject-6252d-default-rtdb.firebaseio.com",
-  projectId: "jsproject-6252d",
-  storageBucket: "jsproject-6252d.appspot.com",
-  messagingSenderId: "161082613703",
-  appId: "1:161082613703:web:173445a7de1f2f45ba03e2",
-  measurementId: "G-Q1Y0MPD1D9",
+  authDomain: import.meta.env.VITE_authDomain,
+  databaseURL: import.meta.env.VITE_databaseURL,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
+  measurementId: import.meta.env.VITE_measurementId,
 };
 
 // Initialize Firebase
